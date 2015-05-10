ac558889c3a81d0a55ad5964d55fbc9fc3fbb81c
Fran Muñoz <framuomo@inf.upv.es>
2015-05-10 02:44:16 +0200

    Adds liveReload

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,43 @@
 var gulp = require('gulp'),
-	sass = require('gulp-ruby-sass') ,
-	notify = require("gulp-notify") ,
-	bower = require('gulp-bower');
+    sass = require('gulp-ruby-sass') ,
+    notify = require("gulp-notify") ,
+    bower = require('gulp-bower');
+
+var liveReload = require('./liveReload.js');
 
 var config = { 
-	sassPath: './resources/sass',
-	 bowerDir: './resources/sass/vendor' 
+    sassPath: './resources/sass',
+     bowerDir: './resources/sass/vendor' 
 }
 
 gulp.task('bower', function() { 
-	return bower() .pipe(gulp.dest(config.bowerDir)) 
+    return bower() .pipe(gulp.dest(config.bowerDir)) 
 });
 
 gulp.task('icons', function() { 
-	return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*') 
-		.pipe(gulp.dest('./public/fonts')); 
+    return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*') 
+        .pipe(gulp.dest('./public/fonts')); 
 });
 
-gulp.task('sass', function () {
-    return sass(config.sassPath + '/style.scss',{
-        	style: 'compressed',
-			 loadPath: [ 
-				'./resources/sass',  
-				config.bowerDir + '/bootstrap-sass-official/assets/stylesheets',  
-				config.bowerDir + '/fontawesome/scss'
-			] 
-        }).on('error', function(error) { 
-			return "Error: " + error.message; 
-		})
+gulp.task('sass', function() {
+    return sass(config.sassPath + '/style.scss', {
+            style: 'compressed',
+             loadPath: [ 
+                './resources/sass',  
+                config.bowerDir + '/bootstrap-sass-official/assets/stylesheets',  
+                config.bowerDir + '/fontawesome/scss'
+            ] 
+        }).on('error', notify.onError(function (error) {
+            return "Error: " + error.message;
+         }))
         .pipe(gulp.dest('./public/css'));
 });
 
-// Rerun the task when a file changes
+gulp.task('install', ['bower', 'icons', 'sass']);
  
-gulp.task('watch', function() { 
-	gulp.watch(config.sassPath + '/**/*.scss', ['sass']); 
+gulp.task('default', function() { 
+  	// Rerun the task when a file changes
+    gulp.watch(config.sassPath + '/**/*.scss', ['sass']); 
+    // LiveReload
+    liveReload.startSync();
 });
-
-  
-gulp.task('default', ['bower', 'icons', 'sass']);
